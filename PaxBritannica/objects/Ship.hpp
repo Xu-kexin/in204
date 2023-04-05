221d840ae2ef3cb064c9b0f975bd28df14936459
Xu-kexin <596059345@qq.com>
2023-04-05 09:06:11 +0200

    delete useless file

--- PaxBritannica/objects/Ship.hpp
+++ PaxBritannica/objects/Ship.hpp
@@ -19,6 +19,7 @@
 
 #include <stdexcept>
 
+
 class Ship {
     friend class Environment;
     friend bool operator==(const Ship& s1, const Ship& s2) {
@@ -29,20 +30,24 @@ class Ship {
     }
 public:
     Ship() {};
+    // constructor with parameters
     Ship(unsigned int id, bool large, int variant, int speed, int health): id(id), large{large}, variant{variant}, speed{speed}, health{health} {
         if (!variant || variant > 4)
             throw std::runtime_error("Only 4 types of ship is supported!");
     }
     
+    // set the position of the ship
     void SetPosition(int x, int y) {
         pos.x = x;
         pos.y = y;
     }
     
+    // update the ship's position by moving it to the left
     void Update() {
         pos.x -= speed;
     }
     
+    // reduce the ship's health by dmg, and return whether the ship has been destroyed
     bool GotHit(int dmg) {
         health -= dmg;
         if (health < 0)
